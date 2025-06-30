1b2b215997b19ace732f2f31c6d226a4413836ad
Timothée Ravier <tim@siosm.fr>
2025-06-30 20:17:29 +0200

    Setup basic integration tests

---
+++ lib/tests/symlink.rs
@@ -0,0 +1,37 @@
+use std::path::Path;
+use std::fs;
+
+#[test]
+fn test_symlink_test1() {
+    let f = Path::new("../test-data/test1");
+
+    let _ = fs::remove_file(f.join("run/extensions/foo.raw"));
+    let _ = fs::remove_file(f.join("run/extensions/test.raw"));
+    assert!(!f.join("run/extensions/foo.raw").exists());
+    assert!(!f.join("run/extensions/test.raw").exists());
+
+    let mut manager = sysexts_manager_lib::manager::new_with_root(f).unwrap();
+    manager.load_config().unwrap();
+    manager.load_images().unwrap();
+    manager.enable().unwrap();
+
+    assert!(f.join("run/extensions/foo.raw").exists());
+    assert!(f.join("run/extensions/test.raw").exists());
+}
+
+#[test]
+fn test_symlink_test2() {
+    let f = Path::new("../test-data/test2");
+
+    let _ = fs::remove_file(f.join("run/extensions/foo.raw"));
+    let _ = fs::remove_file(f.join("run/extensions/test.raw"));
+    assert!(!f.join("run/extensions/foo.raw").exists());
+    assert!(!f.join("run/extensions/test.raw").exists());
+
+    let mut manager = sysexts_manager_lib::manager::new_with_root(f).unwrap();
+    manager.load_config().unwrap();
+    manager.load_images().unwrap();
+    manager.enable().unwrap();
+
+    assert!(f.join("run/extensions/test.raw").exists());
+}
