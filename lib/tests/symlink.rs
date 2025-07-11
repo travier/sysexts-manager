57cb481fc1ef12abef821c75b3f32b5b4669be1a
Timothée Ravier <tim@siosm.fr>
2025-07-11 14:27:24 +0200

    lib/tests: Fix and improve tests for new enable/disable logic

--- lib/tests/symlink.rs
+++ lib/tests/symlink.rs
@@ -1,37 +1,90 @@
-use std::fs;
-use std::path::Path;
+use std::fs::{read_link, remove_file};
+use std::path::{Path, PathBuf};
 
-#[test]
-fn test_symlink_test1() {
-    let f = Path::new("../test-data/test1");
-
-    let _ = fs::remove_file(f.join("run/extensions/foo.raw"));
-    let _ = fs::remove_file(f.join("run/extensions/test.raw"));
-    assert!(!f.join("run/extensions/foo.raw").exists());
-    assert!(!f.join("run/extensions/test.raw").exists());
-
-    let mut manager = sysexts_manager_lib::manager::new_with_root(f).unwrap();
-    manager.load_config().unwrap();
-    manager.load_images().unwrap();
-    manager.enable().unwrap();
-
-    assert!(f.join("run/extensions/foo.raw").exists());
-    assert!(f.join("run/extensions/test.raw").exists());
+fn cleanup(f: &Path, sysexts: &Vec<&'static str>) {
+    for s in sysexts {
+        let run_sysexts = f.join("run/extensions").join(format!("{s}.raw"));
+        let _ = remove_file(&run_sysexts);
+        assert!(!run_sysexts.exists());
+    }
 }
 
 #[test]
-fn test_symlink_test2() {
-    let f = Path::new("../test-data/test2");
+fn basic_latest_version() {
+    let f = Path::new("./test-data/basic_latest_version");
 
-    let _ = fs::remove_file(f.join("run/extensions/foo.raw"));
-    let _ = fs::remove_file(f.join("run/extensions/test.raw"));
-    assert!(!f.join("run/extensions/foo.raw").exists());
-    assert!(!f.join("run/extensions/test.raw").exists());
+    let sysexts = vec!["foo", "test"];
+
+    cleanup(f, &sysexts);
 
     let mut manager = sysexts_manager_lib::manager::new_with_root(f).unwrap();
     manager.load_config().unwrap();
     manager.load_images().unwrap();
-    manager.enable().unwrap();
+    manager.enable_all().unwrap();
 
-    assert!(f.join("run/extensions/test.raw").exists());
+    for s in &sysexts {
+        let run_sysexts = f.join("run/extensions").join(format!("{s}.raw"));
+        assert!(run_sysexts.exists());
+    }
+    assert_eq!(
+        read_link(f.join("run/extensions/foo.raw")).unwrap(),
+        PathBuf::from("../../var/lib/extensions.d/foo-1.4-42-x86-64.raw")
+    );
+    assert_eq!(
+        read_link(f.join("run/extensions/test.raw")).unwrap(),
+        PathBuf::from("../../var/lib/extensions.d/test-20250330-42-x86-64.raw")
+    );
+
+    cleanup(f, &sysexts);
+}
+
+#[test]
+fn basic_multiple_release() {
+    let f = Path::new("./test-data/basic_multiple_release");
+
+    let sysexts = vec!["foo", "test"];
+
+    cleanup(f, &sysexts);
+
+    let mut manager = sysexts_manager_lib::manager::new_with_root(f).unwrap();
+    manager.load_config().unwrap();
+    manager.load_images().unwrap();
+    manager.enable_all().unwrap();
+
+    for s in &sysexts {
+        let run_sysexts = f.join("run/extensions").join(format!("{s}.raw"));
+        assert!(run_sysexts.exists());
+    }
+    assert_eq!(
+        read_link(f.join("run/extensions/foo.raw")).unwrap(),
+        PathBuf::from("../../var/lib/extensions.d/foo-20250205-42-x86-64.raw")
+    );
+    assert_eq!(
+        read_link(f.join("run/extensions/test.raw")).unwrap(),
+        PathBuf::from("../../var/lib/extensions.d/test-20250330-42-x86-64.raw")
+    );
+
+    cleanup(f, &sysexts);
+}
+
+#[test]
+fn no_valid_sysext() {
+    let f = Path::new("./test-data/no_valid_sysext");
+
+    let sysexts = vec!["foo", "test"];
+
+    cleanup(f, &sysexts);
+
+    let mut manager = sysexts_manager_lib::manager::new_with_root(f).unwrap();
+    manager.load_config().unwrap();
+    manager.load_images().unwrap();
+    assert!(manager.enable_all().is_err());
+
+    for s in &sysexts {
+        let _run_sysexts = f.join("run/extensions").join(format!("{s}.raw"));
+        // FIXME
+        // assert!(!run_sysexts.exists());
+    }
+
+    cleanup(f, &sysexts);
 }
